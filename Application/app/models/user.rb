866bbeabff336d0df3d83d8616a8097c44d06d35
th222fa <th222fa@student.lnu.se>
2015-01-30 15:15:32 +0100

    inlogg och registrering

--- Application/app/models/user.rb
+++ Application/app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   has_one :apikey
-  validates :username, length: {in: 3..50}, uniqueness: true
+  validates_length_of :username, within: 3..50, :message => '^Användarnamnet måste vara mellan 3-50 tecken'
+  validates_uniqueness_of :username, message: '^Användarnamnet är upptaget'
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+  validates_length_of :email, maximum: 255, :message => 'Emailadressen får max vara 255 tecken'
+  validates_format_of :email, with: VALID_EMAIL_REGEX, message: '^Ogiltlig email'
+  validates_uniqueness_of :email, case_sensitive: false, message: '^Det finns redan en användare med angiven email'
   has_secure_password
-  validates :password, length: { minimum: 6 }
-  before_save { self.email = email.downcase! }
+  validates_length_of :password, minimum: 6, message: '^Lösenordet måste vara minst 6 tecken'
+  before_save { self.username = username.downcase! }
 end
--- Application/config/routes.rb
+++ Application/config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'user#index'
+   root :to => redirect('/login')
+   get  '/login'   => 'sessions#new'
+   post '/login'   => 'sessions#create'
+
+   resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
